101d2bb43df0731c3e63f81d7df029c91b5d25d1
lukasjanusauskas <lukasjanusauskaslj@gmail.com>
2024-12-28 14:59:40 +0200

    Fix postgres connection acquisition

--- dags/data_imports.py
+++ dags/data_imports.py
@@ -1,15 +1,48 @@
 from collect.collect import SDMXCollector
+
+from sqlalchemy.engine import URL
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
+
 from airflow.models.taskinstance import TaskInstance
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 
-def import_bop_data(ti: TaskInstance):
-  dims = ti.xcom_pull(task_ids='bop_meta', key='bop_dimensions')
-  n_args = len(dims)
-  print(dims)
+def get_conn():
+  hook = PostgresHook(postgres_conn_id='pg_local')
+  return hook.get_conn()
 
-def import_inr_data(ti: TaskInstance):
-  dims = ti.xcom_pull(task_ids='inr_meta', key='inr_dimensions')
+def import_bop_data(
+  source:str,
+  resource: str,
+  flow_ref: str | list[str],
+  ti: TaskInstance,
+  params: dict = None):
 
-  print(dims)
+  dims = ti.xcom_pull(task_ids='bop_meta', 
+                      key='bop_dimensions')
+  n_dims = len(dims)
+
+  collector = SDMXCollector(source, resource)
+  data = collector.make_url(flow_ref,
+                           n_args=n_dims,
+                           params=params)
+
+def import_inr_data(
+  source:str,
+  resource: str,
+  flow_ref: str | list[str],
+  ti: TaskInstance,
+  params: dict = None):
+
+  dims = ti.xcom_pull(task_ids='inr_meta',
+                      key='inr_dimensions')
+  n_dims = len(dims)
+
+  collector = SDMXCollector(source, resource)
+  data = collector.make_url(flow_ref,
+                           n_args=n_dims,
+                           params=params)
 
 def import_exr_data(
   source:str,
@@ -18,8 +51,12 @@ def import_exr_data(
   ti: TaskInstance,
   params: dict = None):
 
-  dims = ti.xcom_pull(task_ids='exr_meta', key='exr_dimensions')
+  con = get_conn()
+
+  dims = ti.xcom_pull(task_ids='exr_meta',
+                      key='exr_dimensions')
 
   collector = SDMXCollector(source, resource)
-  data = collector.make_url(flow_ref, params=params)
-  print(data)
+  data = collector.make_url(flow_ref,
+                           params=params)
+
--- dags/etl_dag.py
+++ dags/etl_dag.py
@@ -3,7 +3,7 @@ from airflow import DAG
 
 from airflow.operators.python import PythonOperator
 from meta_imports import import_bop_meta, import_inr_meta, improt_exr_meta
-from data_imports import import_bop_data
+from data_imports import import_bop_data, import_inr_data, import_exr_data
 
 
 default_args = {
@@ -13,11 +13,11 @@ default_args = {
 }
 
 with DAG(
-  dag_id='forex_etl_v0_04',
+  dag_id='forex_etl_v0_05',
   default_args=default_args,
   description='The first try at a FOREX data ETL',
   start_date=datetime(2024, 12, 1),
-  schedule_interval='@weekly'
+  schedule_interval='@once'
 ) as dag:
 
   # Definition of metadata import tasks
@@ -57,23 +57,42 @@ with DAG(
   # Definition of data import tasks
   bop_import = PythonOperator(
     task_id = "import_bop",
-    python_callable = import_bop_data
+    python_callable = import_bop_data,
+    op_kwargs = {
+      "source": "sdmx.oecd.org",
+      "resource": "public/rest",
+      "flow_ref": ["OECD.SDD.TPS", "DSD_BOP@DF_BOP", ""],
+      "params": {
+        "format": "csv",
+        "startPeriod": "2022-Q4"
+      }
+    }
   )
 
   inr_import = PythonOperator(
     task_id = "import_inr",
-    python_callable = import_bop_data
+    python_callable = import_inr_data,
+    op_kwargs = {
+      "source": "sdmx.oecd.org",
+      "resource": "public/rest",
+      "flow_ref": ["OECD.SDD.STES", "DSD_KEI@DF_KEI", "4.0"],
+      "params": {
+        "format": "csv",
+        "startPeriod": "2022-Q4"
+      }
+    }
   )
 
   exr_import = PythonOperator(
     task_id = "import_exr",
-    python_callable = import_bop_data,
+    python_callable = import_exr_data,
     op_kwargs = {
       "source": "data-api.ecb.europa.eu",
       "resource": "service",
       "flow_ref": "EXR",
       "params": {
-        "startDate"
+        "format": "csvdata",
+        "startPeriod": "2024-12-20"
       }
     }
   )
