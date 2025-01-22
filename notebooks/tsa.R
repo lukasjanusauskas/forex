145b61a2692f004ffee0894c9f7dbd04951d423a
lukasjanusauskas <lukasjanusauskaslj@gmail.com>
2025-01-22 13:35:43 +0200

    Redo of a big cunk of time series analysis

--- notebooks/tsa.R
+++ notebooks/tsa.R
@@ -1,8 +1,8 @@
 # In this file I do time series analysis to:
-# 1. Determine, whether the time series, I am modelling are stationary
-# 2. Explore the cross-correlation
-# 3. Explore the periodicity of the time series
-# 4. Explore time-series outliers 
+# 1. Analize the nature of trends of exchange rates
+# 2. Explore the seasonality
+# 3. Check, whether cross-correlation is good for modelling
+# 3a. If CCF won't be good: do some feature engineering
 
 # Import data ##################################################################
 library(RPostgreSQL)
@@ -28,103 +28,60 @@ master <- dbReadTable(con, 'master')
 inr_names <- dbReadTable(con, "inr_measure_final")
 bop_names <- dbReadTable(con, "bop_measure_final")
 
-# TSA ########################################################################## 
+# TSA ##########################################################################
+# Trend analysis ###############################################################
 
-# Periodicity ##################################################################
+ex_rates %>% 
+  ggplot(aes(x = time_period, y = rate)) +
+    geom_line() +
+    facet_wrap(~currency, scales = "free_y")
 
-p.values.nolag <- ex_rates %>% 
+# Overall, the trends do seem to be different for the different exchange rates:
+# whereas some seem to have exponential trends, others may not even have a clear trend at all
+
+acfs <- ex_rates %>% 
   group_by(currency) %>% 
   arrange(time_period) %>% 
-  group_map(~ adf.test(ts(.x$rate))$p.value)
+  reframe(acf_val = acf(rate, lag.max=5000)$acf,
+          acf_lag = acf(rate, lag.max=5000)$lag)
 
-mean(p.values.nolag < 0.05)
+acfs %>% 
+  ggplot(aes(x = acf_lag, y = acf_val)) +
+    geom_point(size = 0.2) +
+    geom_segment(aes(xend = acf_lag, y = 0, yend = acf_val, col = acf_lag),
+                 linewidth = 0.1, alpha = 0.7) +
+    facet_wrap(~currency) +
+    theme(legend.position = "none")
 
-calc_lagged_ts <- function(x){
-  lagged <- lag(x, default = x[1])
-  return(ts(x - lagged))
-}
+# ACF plots also show the differences between the strength of trends in time-series
+# While some ACFs decay really slowly, others have a, comparatively, fast decaying 
+# Also, models with strong assumptions would have weak longevity, so any model 
+# that makes assumptions about trends shouldn't be considered.
 
-p.values.lag <- ex_rates %>% 
-  group_by(currency) %>% 
-  arrange(time_period) %>% 
-  group_map(~ adf.test(calc_lagged_ts(.x$rate))$p.value)
+# Also, there aren't any clear seasonality, although some ACFs do oscillate, 
+# they do it in a decaying manner.
+ 
+# Seasonality ##################################################################
 
-mean(p.values.lag < 0.05)
-# Only 1/9th of exchange rates pass the Augmented Dickey-Fuller test at 5% level
-# Without any differencing, but first order differencing transformed the data into a 
-# stationary time series
+# For checking the seasonality component, I will use periodograms, to check seasonality
 
 ex_rates %>% 
   group_by(currency) %>% 
-  mutate(rate_diff = rate - lag(rate)) %>% 
-  arrange(time_period) %>% 
-  ggplot(aes(x = time_period, y = rate_diff)) +
+  arrange(currency) %>% 
+  reframe(per = periodogram(rate)$spec[1:10],
+          freq = periodogram(rate)$freq[1:10]) %>% 
+  ggplot(aes(x = freq, y = per)) +
     geom_line() +
-    facet_wrap(~ currency, scale="free_y") +
-    geom_smooth(method = 'lm')
+    facet_wrap(~currency, scales = "free")
 
-# All of the differenced times series are approximately centered around zero.
-# Which is to be excepted, however it means, that we need to construct 
-# a different time series(weekly aggregates for example, or use windowing), 
-# to not get a rubbish forecast.
+decompose(filter(ex_rates, currency == 32)$rate)
 
-# Uni-variate ARIMA forecast ###############################################################
-
-test_sample_arima <- function(x) {
-  model <- auto.arima(x)
-  return( residuals(model) )
-}
-
-lagged <- ex_rates %>% 
-  group_by(currency) %>% 
-  mutate(month_lag = rate - lag(rate, n=10)) %>% 
-  drop_na()
-
-train_df <- lagged %>% 
-  filter(time_period < ymd('2024-11-30'))
-
-test_df <- lagged %>% 
-  filter(time_period >= ymd('2024-11-30'))
-
-currencies <- unique(ex_rates$currency)
-results <- data.frame(list("curr"=NA, "date"=NA, "pred"=NA, "month_lag"=NA))
-
-for (curr in currencies) {
-  train <- train_df %>% 
-    filter(currency == curr) %>%
-    ungroup() %>% 
-    select(month_lag)
-  
-  test <- test_df %>% 
-    filter(currency == curr) %>% 
-    ungroup()
-  
-  if (nrow(test) == 0) {
-    next
-  }
-  
-  model <- auto.arima(train)
-  pred <- predict(model, nrow(test))$pred
-  
-  res <- cbind(rep(curr, nrow(test)), test$time_period, c(pred), c(test$month_lag))
-  colnames(res) <- c("curr", "date", "pred", "month_lag")
-  
-  results <- rbind(results, res)
-}
-
-results %>% 
-  drop_na() %>% 
-  ggplot(aes(x = date, y=month_lag)) +
-    geom_line(col='black') +
-    geom_line(aes(y=pred), col='red') +
-  facet_wrap(~curr, scales='free_y')
-
-# For most currencies, this forecast is horrible, however, I will try to improve on it with XGBoost later
+# What we can see is that there is no long term seasonality,
+# Furthermore, short-term seasonality also does not seem to be present.
 
 # Cross-correlation ############################################################
 
 # Get quarterly ex rates aggregates from master
-# TODO: fix aggregate calculation - it doesn't take into account measures
 
 aggregates <- master %>% 
   drop_na() %>% 
@@ -215,82 +172,5 @@ ccf_results %>%
 # However the sign of the correlation does vary from currency to currency.
 # I expect the tendencies to be roughly the same, when I will calculate the 
 # exchange rates with different denominators and use differences or ratios of the measures. 
-
-# Outlier detection ############################################################
-
-# Big changes in interest rates and exchange rate 
-
-aggregates %>% 
-  inner_join(inr_names, by = join_by(inr_measure == index)) %>%  
-  group_by(currency, inr_measure) %>% 
-  mutate(diff_inr = min_inr - lag(min_inr),
-         date = paste0(`year(date)`, '-Q', `quarter(date)`)) %>% 
-  mutate(date = parse_date_time(date, orders = "%y-%q")) %>% 
-  drop_na() %>% 
-  ggplot(aes(x = date, y = diff_inr, col = as.factor(full_name))) +
-    geom_line(alpha = 0.8) +
-    facet_wrap(~ currency, scales = "free_y") +
-    theme(legend.position = c(0.7, 0.1)) +
-    guides(color = guide_legend(title = "Interest rate measure"))
-
-# All of the entities, have, what seems to be, outliers in the differenced measures.
-# Therefore a univariate outlier detection method will be employed to get these outliers and 
-# See, how do these changes correlate with exchange rates. And then we might be able 
-# to come up with a strategy to compare these measures between entities.
-
-# If the distributions are normal, we may use the z-score method.
-
-aggregates %>% 
-  inner_join(inr_names, by = join_by(inr_measure == index)) %>%  
-  group_by(currency, inr_measure) %>% 
-  mutate(diff_inr = min_inr - lag(min_inr),
-         date = paste0(`year(date)`, '-Q', `quarter(date)`)) %>% 
-  mutate(date = parse_date_time(date, orders = "%y-%q")) %>% 
-  drop_na() %>% 
-  ggplot(aes(x = diff_inr, fill = as.factor(full_name))) +
-    geom_histogram(position = "dodge") +
-    facet_wrap(~ currency, scales = "free_y") +
-    theme(legend.position = c(0.7, 0.1)) +
-    guides(color = guide_legend(title = "Interest rate measure"))
-
-# The distributions are definetly not Gaussian
-# Therefore I will use the IQR criterion(default boxplot.stats criterion)
-
-currencies <- unique(aggregates$currency)
-sample <- aggregates %>% 
-  filter(inr_measure == 3)
-
-outlier_tested <- aggregates %>% 
-  group_by(currency, inr_measure) %>% 
-  mutate(
-    iqr = IQR(min_inr),
-    q1 = quantile(min_inr, 0.25),
-    q3 = quantile(min_inr, 0.75)
-  ) %>% 
-  mutate(is_inr_outlier = 
-           (min_inr < q1 - 1.5 * iqr) |
-           (min_inr > q3 + 1.5 * iqr)) %>% 
-  select(-c("iqr", "q1", "q3"))
-
-outlier_tested %>%
-  mutate(date = parse_date_time(
-    paste0(`year(date)`, "-Q", `quarter(date)`),
-    orders = "%y-%q"
-  )) %>% 
-  filter(inr_measure == 3) %>%  # Short-term interest rates
-  ggplot(aes(x = date, y = mean_rate, col = is_inr_outlier)) +
-    geom_line() +
-    facet_wrap(~ currency, scales = "free_y") +
-    theme(legend.position = c(0.7, 0.1))
-
-# Interest rate outliers can indicate different trajectories for exchange rates,
-# However, not all of them are explained or effected by them, so we need some more data
-
-# 
-
-
-# Periodicity ##################################################################
-
-
 
 
