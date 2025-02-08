4fa5090f25fe76689f331972ab4b97dfa951f945
lukasjanusauskas <lukasjanusauskaslj@gmail.com>
2025-02-08 17:59:11 +0200

    Added textual information and restructured the dashboard

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,8 +1,6 @@
 from dash import Dash, html, dcc, callback, Input, Output
 from data import DataPlotter
 
-from datetime import timedelta
-
 
 app = Dash(
   external_scripts=[{"src": "https://cdn.tailwindcss.com"}]
@@ -48,16 +46,30 @@ app.layout = [
             {'label': html.Span('MAX', className='ml-2 mr-4'), 'value': -1}
           ],
           id='time_frame_buttons',
-          value=7,
+          value=365,
           inline=True
         )
       ]
      ),
 
-     html.Div(className="flex flex-row gap-20",
-        children= [dcc.Graph(id="ex_rate_graph", className="w-2/5"),
-                   dcc.Graph(id="forecast_graph", className="w-2/5")]
-     )
+     html.Div(className="flex flex-col gap-20",
+        children= [
+          html.Div(className='flex flex-row gap-5', id='historical_panel',
+            children=[dcc.Graph(id="ex_rate_graph", className="w-1/1")]
+          ),
+          html.Div(className='flex flex-row gap-5', id='forecast_panel',
+            children=[
+              dcc.Graph(id="forecast_graph"),
+              html.P(id='forecast_one_day', className='w-1/4'),
+              html.P(id='forecast_one_week', className='w-1/4')
+            ]
+          )
+        ]
+     ),
+
+      html.Div(className='bg-slate-900 mx-auto w-full',
+               children=html.P('Lukas Janušauskas')
+      )
     ])
 ]
 
@@ -73,13 +85,34 @@ def update_ex_rate(curr_1: str, curr_2: str,
 
   return plotter.get_ex_rate_graph(curr_2, curr_1, timeframe)
 
+"""
+TODO: implement this function, that updates multiple graphs, related to each other
+
 @callback(
-  Output('forecast_graph', 'figure'),
+# Output: Div children: a figure for exchange rates and macro economic data
+  Output('historical_panel', 'children'),
+# Input: currency 1, currency 2, time range
   Input('dropdown-currency1', 'value'),
-  Input('dropdown-currency2', 'value')
+  Input('dropdown-currency2', 'value'),
+  Input('time_frame_buttons', 'value')
 )
-def update_forecast(curr_1: str, curr_2: str):
-  return plotter.plot_forecast(curr_2, curr_1)
+def update_historical_panel(curr_1: str, curr_2: str,
+                            timeframe: int):
+
+  return plotter.update_historical(curr_2, curr_1, timeframe)
+"""
+
+@callback(
+# Output: forecast panel, that has both the forecast line graph and the text
+  [Output('forecast_one_day', 'children'),
+  Output('forecast_one_week', 'children'),
+  Output('forecast_graph', 'figure')],
+
+  [Input('dropdown-currency1', 'value'),
+  Input('dropdown-currency2', 'value')]
+)
+def update_forecast_panel(curr_1: str, curr_2: str):
+  return plotter.update_forecast_panel(curr_1, curr_2)
 
 if __name__ == '__main__':
   app.run(debug=True)
--- dashboard/data.py
+++ dashboard/data.py
@@ -1,7 +1,15 @@
-import plotly.graph_objects as go
+
+  # TODO:
+  # 1. Add Balance of payment data
+  # 2. Add interest rate data
+  # 3. Add text data, that would display the forecasts (and voliatilty?) in textual format
+
 import pandas as pd
 from numpy import ones_like
 from datetime import datetime, timedelta
+
+from dash import html
+import plotly.graph_objects as go
 import plotly.express as px
 
 from sqlalchemy import create_engine
@@ -18,11 +26,22 @@ class DataPlotter:
       # Import data tables
       self.pair_df = pd.read_sql_query('SELECT * FROM ex_pairs', con=connection)\
                        .groupby(['currency_1', 'currency_2'])
+
+      # Exchange rate data
       self.ex_rates = pd.read_sql_query('SELECT * FROM ex_rates', con=connection)\
                         .groupby('currency')
-      self.macro = pd.read_sql('SELECT * FROM macro', con=connection)
+
+      # All macroeconomic data for the second part of the dashboard:
+      # We will create a mini-dashboard for all present additional info on the currency
+      # For example, if we have chosen GBP we may show UK data
+      self.master = pd.read_sql_query('SELECT * FROM master', con=connection)\
+                      .groupby('currency')
+ 
+      # Forecasts
       self.forecast = pd.read_sql('SELECT * FROM forecast', con=connection)\
                         .groupby('currency')
+
+      # TODO after update dag: pick the last week forecasts only in here
 
       # Import the dimension tables
       self.bop_measure = pd.read_sql_query('SELECT * FROM bop_measure_final', con=connection)\
@@ -55,7 +74,22 @@ class DataPlotter:
 
     except KeyError:
       return
+
+  def __handle_none(self, message: str):
+    return go.Figure()\
+      .update_layout(showlegend=False,
+                     plot_bgcolor='rgba(0, 0, 0, 0)')\
+      .add_annotation(x=2, y=2, text=message,
+                      font={"size": 30, "color": "red"},
+                      showarrow=False)\
+      .update_xaxes(visible=False)\
+      .update_yaxes(visible=False)
   
+  # For plotting historical data ####################################################################
+  # Plots past data of:
+  #   1. FOREX rate
+  #   2. Interest rates
+  #   3. Balance of payments
   def get_ex_rate_graph(self, curr_1: str, curr_2: str, timeframe: int):
     """
     Creates a graph of the exchange rates, given two currencies and a time frame(integer in days).
@@ -89,6 +123,9 @@ class DataPlotter:
     earliest = datetime.today() - timedelta(days=timeframe+7) 
     mask = (data['date'] >= earliest if timeframe != -1 else
             ones_like(data['date'].values, dtype=bool))
+ 
+    if sum(mask) == 0:
+      return self.__handle_none('There is no data in this date range')
 
     return px.line(
       data[mask].sort_values('date'),
@@ -105,14 +142,7 @@ class DataPlotter:
     ent = self.get_currency_index(curr)
 
     if not ent:
-      return go.Figure()\
-        .update_layout(showlegend=False,
-                       plot_bgcolor='rgba(0, 0, 0, 0)')\
-        .add_annotation(x=2, y=2, text='No data available',
-                        font={"size": 50, "color": "red"},
-                        showarrow=False)\
-        .update_xaxes(visible=False)\
-        .update_yaxes(visible=False)
+      return self.__handle_none('No data available')
 
     data = self.ex_rates.get_group(ent)
     earliest = datetime.today() - timedelta(days=timeframe+7)
@@ -120,6 +150,9 @@ class DataPlotter:
     # -1 in this context is a special value meaning: maximum days
     mask = (data['time_period'] >= earliest if timeframe != -1 else
             ones_like(data['time_period'].values, dtype=bool))
+      
+    if sum(mask) == 0:
+      return self.__handle_none('There is no data in this date range')
 
     return px.line(
       data[mask].sort_values('time_period'),
@@ -131,6 +164,36 @@ class DataPlotter:
       },
       title=f'Exchange rate of {curr}/EUR'
     )
+
+  # For plotting forecast data ##################################################################
+  # Gets the forecasts from the pre-computed forecasts
+  # Loads it into a line graph and displays the one-day and one-week forecasts.
+
+  def update_forecast_panel(self, curr_1, curr_2):
+    data, line = self.plot_forecast(curr_1, curr_2)
+
+    one_day = data.values[0]
+    one_week = data.values[-1]
+
+    one_day_output = html.Div(className='flex flex-col justify-center items-center mmy-auto h-full',
+      children=[
+        html.P(
+          f'Tommorow:', className='text-3xl font-bold'),
+        html.P(
+          f'{one_day:.6f}', className='text-xl font-thin')
+      ]
+    )
+
+    one_week_output = html.Div(className='flex flex-col justify-center items-center my-auto h-full',
+      children=[
+        html.P(
+          f'Next week:', className='text-3xl font-bold'),
+        html.P(
+          f'{one_week:.6f}', className='text-xl font-thin')
+      ]
+    )
+
+    return [one_day_output, one_week_output, line]
 
   def plot_forecast(self, curr_1, curr_2):
     if curr_1 == "EUR":
@@ -145,7 +208,7 @@ class DataPlotter:
     forecast_2 = self.get_forecast(curr_2)
 
     if forecast_1 is None or forecast_2 is None:
-      return 
+      return self.__handle_none('No forecast available')
 
     forecast_1, forecast_2 = forecast_1.set_index(['index']), forecast_2.set_index(['index'])
 
@@ -153,7 +216,7 @@ class DataPlotter:
     forecast_1['fore2'] = forecast_2.loc[:, '0']
     forecast_1['fore'] = forecast_1['fore1'] / forecast_1['fore2']
 
-    return px.line(
+    line = px.line(
       forecast_1.reset_index(),
       x='index',
       y='fore',
@@ -163,21 +226,16 @@ class DataPlotter:
       },
       title='Forecast'
     )
+
+    return (forecast_1['fore'], line)
   
   def plot_forecast_euro(self, curr: str):
     forecast = self.get_forecast(curr)
 
     if forecast is None:
-      return go.Figure()\
-        .update_layout(showlegend=False,
-                       plot_bgcolor='rgba(0, 0, 0, 0)')\
-        .add_annotation(x=2, y=2, text='No forecast available',
-                        font={"size": 50, "color": "red"},
-                        showarrow=False)\
-        .update_xaxes(visible=False)\
-        .update_yaxes(visible=False)
+      return (None, self.__handle_none('No forecast available'))
 
-    return px.line(
+    line = px.line(
       forecast,
       x='index',
       y='0',
@@ -187,6 +245,14 @@ class DataPlotter:
       },
       title='Forecast'
     )
+
+    return (forecast['0'], line)
+
+  def write_forecast(self, curr_1, curr_2) -> pd.DataFrame:
+    # IF one of them is EURO get the forecast of the other
+
+    # Else 
+    pass
 
   def get_forecast(self, curr) -> pd.DataFrame | None:
     try:
@@ -201,5 +267,4 @@ class DataPlotter:
 
 if __name__ == "__main__":
   plotter = DataPlotter("postgresql://airflow:airflow@localhost:5454/forex")
-  fig = plotter.plot_forecast('BRL', 'EUR')
-  fig.show()
+  plotter.show_forecast('BRL', 'EUR')
